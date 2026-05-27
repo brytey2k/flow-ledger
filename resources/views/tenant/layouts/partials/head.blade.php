de06c248f7f3644beb9c7046ca4f9f571e9006ef
Bright Nkrumah <brytey2k@gmail.com>
2026-05-27 08:16:18 +0000

    assets

--- resources/views/tenant/layouts/partials/head.blade.php
+++ resources/views/tenant/layouts/partials/head.blade.php
@@ -17,10 +17,12 @@
 <meta content="Metronic - Tailwind CSS " property="og:title" />
 <meta content="" property="og:description" />
 <meta content="{{ asset('assets/media/app/og-image.png') }}" property="og:image" />
-<link href="{{ asset('assets/media/app/apple-touch-icon.png') }}" rel="apple-touch-icon" sizes="180x180" />
-<link href="{{ asset('assets/media/app/favicon-32x32.png') }}" rel="icon" sizes="32x32" type="image/png" />
-<link href="{{ asset('assets/media/app/favicon-16x16.png') }}" rel="icon" sizes="16x16" type="image/png" />
-<link href="{{ asset('assets/media/app/favicon.ico') }}" rel="shortcut icon" />
+<link href="{{ asset('assets/media/app/favicon.ico') }}?v=3" rel="icon" type="image/x-icon" />
+<link href="{{ asset('assets/media/app/favicon-16x16.png') }}?v=3" rel="icon" sizes="16x16" type="image/png" />
+<link href="{{ asset('assets/media/app/favicon-32x32.png') }}?v=3" rel="icon" sizes="32x32" type="image/png" />
+<link href="{{ asset('assets/media/app/favicon-48x48.png') }}?v=3" rel="icon" sizes="48x48" type="image/png" />
+<link href="{{ asset('assets/media/app/apple-touch-icon.png') }}?v=3" rel="apple-touch-icon" />
+<link href="{{ asset('site.webmanifest') }}?v=3" rel="manifest" />
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
 <link href="{{ asset('assets/vendors/apexcharts/apexcharts.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/vendors/keenicons/styles.bundle.css') }}" rel="stylesheet" />
